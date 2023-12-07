c729d15dd25651021dcb8a1d751285418f96161f
Dmitry Ivanov <dmitsi@rambler.ru>
2023-12-07 16:31:30 +0300

    tailor

--- Utility/user_constants.h
+++ Utility/user_constants.h
@@ -8,6 +8,7 @@
 namespace utl{ 
     constexpr float PIx1 = 3.14159265358979;
     constexpr float PIx2 = 2.*PIx1;    
+    constexpr float PIdiv2 = PIx1/2.0;    
     constexpr float SQRT2 = 1.414213562373095;
     constexpr float SQRT3 = 1.732050807568877;
     constexpr float f = 50.;
